ba148a39bb06a6c065c06db0b32977491a83419a
Boris Diakur <borisdiakur@gmail.com>
2023-02-14 14:40:11 +0100

    🥷🏻

--- src/background.js
+++ src/background.js
@@ -1,22 +1,3 @@
-/**
- * @param {number} tabId - the tab id.
- * @returns {chrome.scripting.CSSInjection} the host.
- */
-function getCssInjection(tabId) {
-  return {
-    target: { tabId, allFrames: false },
-    files: ['src/style.css'],
-  }
-}
-
-/**
- * @param {string} url - the url.
- * @returns {string} the host.
- */
-function getHostname(url) {
-  return new URL(url).hostname
-}
-
 const iconDefault = {
   path: {
     16: '../icons/icon16.png',
@@ -35,80 +16,37 @@ const iconEnabled = {
   },
 }
 
-const tabs = new Map()
-
-chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+chrome.tabs.onUpdated.addListener(async (tabId) => {
   try {
-    if (changeInfo.status == 'complete' && tab.active) {
-      const cssInjection = getCssInjection(tabId)
-      const url = tab.url || tab.pendingUrl
-      if (!url || !url.startsWith('http')) {
-        await chrome.action.setIcon(iconDefault)
-        return
-      }
-      const host = getHostname(url)
-      const storage = await chrome.storage.local.get([host])
-      if (storage[host]) {
-        await chrome.scripting.insertCSS(cssInjection)
-        await chrome.action.setIcon(iconEnabled)
-        tabs.set(tabId, true)
-        return
-      }
-      await chrome.scripting.removeCSS(cssInjection)
-      await chrome.action.setIcon(iconDefault)
-      tabs.delete(tabId)
-    }
+    await chrome.tabs.sendMessage(tabId, { action: 'ninja-mode-update' })
   } catch (err) {
-    console.trace(err)
+    await chrome.action.setIcon(iconDefault)
   }
 })
 
 chrome.tabs.onActivated.addListener(async () => {
   try {
     const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true })
-    const url = tab.url || tab.pendingUrl
-    if (!url || !url.startsWith('http')) {
-      await chrome.action.setIcon(iconDefault)
-      return
-    }
-    const cssInjection = getCssInjection(tab.id)
-    const host = getHostname(tab.url)
-    const storage = await chrome.storage.local.get([host])
-    if (storage[host]) {
-      await chrome.action.setIcon(iconEnabled)
-      if (!tabs.has(tab.id)) {
-        await chrome.scripting.insertCSS(cssInjection)
-        tabs.set(tab.id, true)
-      }
-    } else {
-      await chrome.action.setIcon(iconDefault)
-      await chrome.scripting.removeCSS(cssInjection)
-      tabs.delete(tab.id)
-    }
+    await chrome.tabs.sendMessage(tab.id, { action: 'ninja-mode-update' })
   } catch (err) {
-    console.trace(err)
+    await chrome.action.setIcon(iconDefault)
   }
 })
 
 chrome.action.onClicked.addListener(async (tab) => {
   try {
-    const url = tab.url || tab.pendingUrl
-    if (!url || !url.startsWith('http')) return
-    const cssInjection = getCssInjection(tab.id)
-    const host = getHostname(url)
-    const storage = await chrome.storage.local.get([host])
-    if (storage[host]) {
-      await chrome.action.setIcon(iconDefault)
-      await chrome.storage.local.remove(host)
-      await chrome.scripting.removeCSS(cssInjection)
-      tabs.delete(tab.id)
-    } else {
-      await chrome.action.setIcon(iconEnabled)
-      await chrome.storage.local.set({ [host]: true })
-      await chrome.scripting.insertCSS(cssInjection)
-      tabs.set(tab.id, true)
-    }
+    await chrome.tabs.sendMessage(tab.id, { action: 'ninja-mode-toggle' })
   } catch (err) {
-    console.trace(err)
+    await chrome.action.setIcon(iconDefault)
+  }
+})
+
+chrome.runtime.onMessage.addListener(async (msg) => {
+  if (msg.action === 'ninja-status') {
+    if (msg.data) {
+      await chrome.action.setIcon(iconEnabled)
+    } else {
+      await chrome.action.setIcon(iconDefault)
+    }
   }
 })
---
+++ src/content.js
@@ -0,0 +1,55 @@
+function addNinjaMode() {
+  if (document.getElementById('ninja-mode')) return
+
+  const style = document.createElement('style')
+  style.id = 'ninja-mode'
+  style.innerText = `
+    * {
+      color-scheme: none !important;
+    }
+    :where(html) {
+      color: black;
+    }
+    html {
+      filter: invert(0.9) hue-rotate(180deg) !important;
+      color-scheme: dark !important;
+    }
+    :is(img, video, image, [style*='background-image: url'], [style*='background: url']) {
+      filter: invert(1) hue-rotate(180deg) contrast(1.1) !important;
+    }
+  `
+  document.documentElement.appendChild(style)
+}
+
+function removeNinjaMode() {
+  const style = document.getElementById('ninja-mode')
+  if (style) style.remove()
+}
+
+function updateNinjaMode() {
+  const ninjaModeEnabled = localStorage.getItem('ninja-mode')
+  if (ninjaModeEnabled) {
+    addNinjaMode()
+  } else {
+    removeNinjaMode()
+  }
+  chrome.runtime.sendMessage({ action: 'ninja-status', data: ninjaModeEnabled })
+}
+
+chrome.runtime.onMessage.addListener((msg) => {
+  switch (msg.action) {
+    case 'ninja-mode-toggle':
+      if (localStorage.getItem('ninja-mode')) {
+        localStorage.removeItem('ninja-mode')
+      } else {
+        localStorage.setItem('ninja-mode', 'on')
+      }
+      updateNinjaMode()
+      break
+    case 'ninja-mode-update':
+      updateNinjaMode()
+      break
+  }
+})
+
+updateNinjaMode()
