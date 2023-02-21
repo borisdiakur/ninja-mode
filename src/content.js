920eff6cf0b1ff4898dd1271c6f429c3129dd9aa
Boris Diakur <borisdiakur@gmail.com>
2023-02-21 17:39:50 +0100

    🥷🏻

--- src/content.js
+++ src/content.js
@@ -15,7 +15,7 @@ function addNinjaMode() {
       filter: invert(0.9) hue-rotate(180deg) !important;
       color-scheme: dark !important;
     }
-    :is(img:not([src$='.svg']), video, iframe, image, [style*='background-image: url']:not([style*='data:image/gif'], [style*='data:image/svg']), [style*='background: url']:not([style*='data:image/gif'], [style*='data:image/svg'])) {
+    :is(img:not([src*='svg']), video, iframe, image, [style*='background-image: url']:not([style*='data:image/gif'], [style*='data:image/svg']), [style*='background: url']:not([style*='data:image/gif'], [style*='data:image/svg'])) {
       filter: invert(1) hue-rotate(180deg) contrast(1.1) !important;
     }
   `
